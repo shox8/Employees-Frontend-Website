efcb93e2aca97a8da477e64903cd97b6cebeec4c
Shoxruh <codershoxruh@gmail.com>
2024-03-24 16:58:04 +0500

    Login Done

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,30 @@
+import { Card, Form, Row, Space, Typography } from "antd";
 import { Layout } from "../../components/layout";
+import { CustomInput } from "../../components/custom-input";
+import { PasswordInput } from "../../components/password-input";
+import { CustomButton } from "../../components/custom-button";
+import { Paths } from "../../paths";
+import { Link } from "react-router-dom";
 
 export const Login = () => {
   return (
     <Layout>
-      <div>Login</div>
+      <Row align={"middle"} justify={"center"}>
+        <Card title={"Войдите"} style={{ width: "30rem" }}>
+          <Form onFinish={() => null}>
+            <CustomInput type="email" name="email" placeholder="Email" />
+            <PasswordInput name="password" placeholder="Пароль" />
+            <CustomButton type="primary" htmlType="submit">
+              Войти
+            </CustomButton>
+          </Form>
+          <Space direction="vertical" size="large">
+            <Typography.Text>
+              Нет аккаунта? <Link to={Paths.register}>Зарегистрируйтесь</Link>
+            </Typography.Text>
+          </Space>
+        </Card>
+      </Row>
     </Layout>
   );
 };
