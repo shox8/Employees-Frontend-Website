03a786fc7fca2901af0957745cd785a60cab28e1
Shoxruh <codershoxruh@gmail.com>
2024-03-27 07:23:17 +0500

    Project Done

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,18 +1,19 @@
 import { User } from "../../types";
 import { api } from "./api";
 
+export type UserData = Omit<User, "id">;
 type ResponseLoginData = User & { token: string };
 
 export const authApi = api.injectEndpoints({
   endpoints: (builder) => ({
-    login: builder.mutation<ResponseLoginData, User>({
+    login: builder.mutation<ResponseLoginData, UserData>({
       query: (userData) => ({
         url: "/user/login",
         method: "POST",
         body: userData,
       }),
     }),
-    register: builder.mutation<ResponseLoginData, User>({
+    register: builder.mutation<ResponseLoginData, UserData>({
       query: (userData) => ({
         url: "/user/register",
         method: "POST",
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,21 +1,54 @@
+import { useState } from "react";
 import { Card, Form, Row, Space, Typography } from "antd";
 import { Layout } from "../../components/layout";
 import { CustomInput } from "../../components/custom-input";
 import { PasswordInput } from "../../components/password-input";
 import { CustomButton } from "../../components/custom-button";
 import { Paths } from "../../paths";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../features/auth/authSlice";
+import { useRegisterMutation } from "../../app/services/auth";
+import { User } from "../../types";
+import { isErrorWithMessage } from "../../utils/is-error-with-message";
+import { ErrorMessage } from "../../components/error-message";
+
+type RegisterData = Omit<User, "id"> & { confirmPassword: string };
 
 export const Register = () => {
+  const navigate = useNavigate();
+  const user = useSelector(selectUser);
+  const [error, setError] = useState("");
+  const [registerUser] = useRegisterMutation();
+
+  const register = async (data: RegisterData) => {
+    try {
+      await registerUser(data).unwrap();
+
+      navigate(Paths.home);
+    } catch (error) {
+      const maybeError = isErrorWithMessage(error);
+
+      if (maybeError) {
+        setError(error.data.message);
+      } else {
+        setError("Неизвестная ошибка");
+      }
+    }
+  };
+
   return (
     <Layout>
       <Row align={"middle"} justify={"center"}>
         <Card title={"Зарегистрируйтесь"} style={{ width: "25rem" }}>
-          <Form onFinish={() => null}>
-            <CustomInput  name="name" placeholder="Имя" />
+          <Form onFinish={register}>
+            <CustomInput name="name" placeholder="Имя" />
             <CustomInput type="email" name="email" placeholder="Email" />
             <PasswordInput name="password" placeholder="Пароль" />
-            <PasswordInput name="confirmPassword" placeholder="Повторите пароль" />
+            <PasswordInput
+              name="confirmPassword"
+              placeholder="Повторите пароль"
+            />
             <CustomButton type="primary" htmlType="submit">
               Зарегистрироваться
             </CustomButton>
@@ -24,6 +57,7 @@ export const Register = () => {
             <Typography.Text>
               Уже зарегистрированы? <Link to={Paths.login}>Войдите</Link>
             </Typography.Text>
+            <ErrorMessage message={error} />
           </Space>
         </Card>
       </Row>
